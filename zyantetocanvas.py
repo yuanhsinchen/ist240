2d09c25233d3b5aa0d74cd94bf6250b5cbbf0955
Yuan-Hsin Chen <yuanlmm@gmail.com>
2015-01-30 22:36:32 -0500

    normalize grade to 30 and match by either name or email

--- zyantetocanvas.py
+++ zyantetocanvas.py
@@ -1,19 +1,23 @@
 import csv
 
 zyante = {}
+zyante_name = {}
 # csv column layout on Zyante: 'Last name', 'First name', 'Email', 'Total'
-with open("PennStateIST240Spring2015_report_2015-01-08_0042.csv", 'r') as zf:
+with open("../grade/PennStateIST240Spring2015_report_2015-01-22_1100.csv", 'r') as zf:
     reader = csv.reader(zf)
     # skip the first row of csv which contains headers
     next(reader)
     for row in reader:
         # eamil as a key
-        zyante[row[2].strip()] = float(row[3])
+        zyante[row[2].strip()] = float(row[3]) * 30 / 100
+        # name as a key
+        name = row[0] + ', ' + row[1]
+        zyante_name[name] = float(row[3]) * 30 / 100
 zf.close()
 
 canvas = {}
 # csv column layout on Canvas:'Student', 'ID', 'SIS User ID', 'SIS Login ID'
-with open('Grades-Introduction_to_Computer_Languages_(402748,442069).csv', 'r') as cf:
+with open('../grade/Grades-Introduction_to_Computer_Languages_(402748,442069).csv', 'r') as cf:
     reader = csv.reader(cf)
     # skip the first two row of csv which contains headers
     next(reader)
@@ -21,9 +25,12 @@ with open('Grades-Introduction_to_Computer_Languages_(402748,442069).csv', 'r') 
     for row in reader:
         if row[2].strip() in zyante.keys():
             canvas[row[2].strip()] = {'Student':row[0].strip(), 'ID':row[1].strip(), 'Section':row[4].strip(), 'Read chapters 1-3 and do exercises (6566548)':zyante[row[2].strip()]}
+        elif row[0].strip() in zyante_name:
+            canvas[row[2].strip()] = {'Student':row[0].strip(), 'ID':row[1].strip(), 'Section':row[4].strip(), 'Read chapters 1-3 and do exercises (6566548)':zyante_name[row[0].strip()]}
         else:
             # students who haven't register on Zyante yet
-            print row[0]
+            print row[0], row[2]
+            canvas[row[2].strip()] = {'Student':row[0].strip(), 'ID':row[1].strip(), 'Section':row[4].strip(), 'Read chapters 1-3 and do exercises (6566548)':0}
 cf.close()
 
 with open('hw1_grade.csv', 'w') as of:
@@ -34,6 +41,3 @@ with open('hw1_grade.csv', 'w') as of:
     for row in canvas:
         writer.writerow(canvas[row])
 of.close()
-
-
-
